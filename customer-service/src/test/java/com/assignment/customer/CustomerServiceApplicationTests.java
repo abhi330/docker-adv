287bdbd298d14a55584ec807c7945193610a3580
Medaboina Abhilash <medaboina.abhilash@nagarro.com>
2025-02-02 23:27:34 +0530

    cicd pipeline  changes

--- customer-service/src/test/java/com/assignment/customer/CustomerServiceApplicationTests.java
+++ customer-service/src/test/java/com/assignment/customer/CustomerServiceApplicationTests.java
@@ -2,12 +2,13 @@ package com.assignment.customer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-class CustomerServiceApplicationTests {
-
+@TestPropertySource(locations = "classpath:application-test.yml")
+public class CustomerServiceApplicationTests {
 	@Test
 	void contextLoads() {
+		// Test if Spring Boot context loads successfully
 	}
-
 }
